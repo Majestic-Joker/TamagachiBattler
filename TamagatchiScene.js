5779d2625d5fb7b81d01ec0f7c35f2915e68b391
Alexanderryan133 <alexanderryan133@gmail.com>
2021-06-15 21:25:54 -0500

    Basic
    Created constructor and added basic variables.

---
+++ TamagatchiScene.js
@@ -0,0 +1,22 @@
+class TamagatchiScene extends Phaser.js {
+    constructor () {
+        super ("TamagatchiScene")
+        this.hunger
+        this.happines
+        this.clean
+        this.name = 'Gary'
+    }
+
+    preload () {
+
+    }
+
+    update () {
+
+    }
+
+    create () {
+
+    }
+
+}
