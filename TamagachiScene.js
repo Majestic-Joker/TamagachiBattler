c2f5735e3cc49414c52e67ba6d7e3cd35600caea
Alexanderryan133 <alexanderryan133@gmail.com>
2021-06-16 00:34:00 -0500

    Added buttons to tamagachi
    added buttons to increase food, cleanliness, happiness, and a button to go battle

--- TamagachiScene.js
+++ TamagachiScene.js
@@ -7,10 +7,6 @@ class TamagachiScene extends Phaser.Scene {
         this.name = 'Gary';
     }
 
-    preload () {
-
-    }
-
     update () {
 
     }
@@ -24,12 +20,39 @@ class TamagachiScene extends Phaser.Scene {
         homeBGM.play();
         //make a button for title scene
         let titleBtn = this.add.rectangle(225,400, 200, 100, 0x0000FF);
-        titleBtn.setOrigin(.5);
-        titleBtn.setInteractive();
-        titleBtn.on('pointerdown', () => {
-            homeBGM.stop();
-            this.scene.start("TitleScene");
+            titleBtn.setOrigin(.5);
+            titleBtn.setInteractive();
+            titleBtn.on('pointerdown', () => {
+                homeBGM.stop();
+                this.scene.start("TitleScene");
         });
+
+        let foodBtn = this.add.rectangle(75,60, 75, 75, 0xFFFFFF);
+            foodBtn.setInteractive();
+            foodBtn.on('pointerdown', ()=> {
+            hunger = 100;
+        })
+
+        let toyBtn = this.add.rectangle(175,60, 75, 75, 0xFFFFFF);
+            toyBtn.setInteractive();
+            toyBtn.on('pointerdown', ()=> {
+            this.happines = 100;
+        })
+
+
+        let cleanBtn = this.add.rectangle(275,60, 75, 75, 0xFFFFFF);
+        cleanBtn.setInteractive();
+        cleanBtn.on('pointerdown', ()=> {
+            clean = 100;
+        })
+
+
+        let battleBtn = this.add.rectangle(375,60, 75, 75, 0xFFFFFF);
+        battleBtn.setInteractive();
+        battleBtn.on('pointerdown', ()=> {
+            homeBGM.stop();
+            this.scene.start("BattleScene")
+        })
     }
 
 }
