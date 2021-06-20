2ece4b15e2a94da09b6edf91632dfb2e3634f59f
Joshua Beaty <joshua.a.beaty@gmail.com>
2021-06-20 16:24:48 -0500

    Config - Move and Monster, added 4 monsters and 5 moves

--- monster.js
+++ monster.js
@@ -2,10 +2,12 @@ const MONSTERS = [
     /* EXAMPLE
     {
         //config stuff
-        name: "bat",
-        image: "bat.png",
+        name: "",
+        image: "",
+        backImage: "",
 
         //visible stuff
+        level: 1,
         hp: 1,
         energy: 1,
         attack: 1,
@@ -22,7 +24,7 @@ const MONSTERS = [
         growthAttack: 1,
         growthDefense: 1,
         growthElementalAttack: 1,
-        growthElementalAttack: 1,
+        growthElementalDefense: 1,
         growthSpeed: 1,
 
         //move stuff
@@ -32,4 +34,136 @@ const MONSTERS = [
         move4: ""
     }
     */
+    {
+        //config stuff
+        name: "Baby Flame",
+        image: "fire",
+        backImage: "fireBack",
+
+        //visible stuff
+        level: 1,
+        hp: 10,
+        energy: 10,
+        attack: 4,
+        defense: 5,
+        elementalAttack: 8,
+        elementalDefense: 6,
+        speed: 7,
+        type1: "fire",
+        type2: "",
+
+        //invisible stuff
+        growthHp: 2,
+        growthEnergy: 3,
+        growthAttack: 3,
+        growthDefense: 2,
+        growthElementalAttack: 5,
+        growthElementalDefense: 5,
+        growthSpeed: 2,
+
+        //move stuff
+        move1: 1,
+        move2: 0,
+        move3: "",
+        move4: ""
+    },
+    {
+        //config stuff
+        name: "Baby Earth",
+        image: "earth",
+        backImage: "earthBack",
+
+        //visible stuff
+        level: 1,
+        hp: 10,
+        energy: 10,
+        attack: 9,
+        defense: 8,
+        elementalAttack: 5,
+        elementalDefense: 6,
+        speed: 4,
+        type1: "earth",
+        type2: "",
+
+        //invisible stuff
+        growthHp: 3,
+        growthEnergy: 2,
+        growthAttack: 5,
+        growthDefense: 5,
+        growthElementalAttack: 2,
+        growthElementalDefense: 3,
+        growthSpeed: 2,
+
+        //move stuff
+        move1: 2,
+        move2: 0,
+        move3: "",
+        move4: ""
+    },
+    {
+        //config stuff
+        name: "Baby Wind",
+        image: "wind",
+        backImage: "windBack",
+
+        //visible stuff
+        level: 1,
+        hp: 10,
+        energy: 10,
+        attack: 3,
+        defense: 5,
+        elementalAttack: 7,
+        elementalDefense: 6,
+        speed: 9,
+        type1: "wind",
+        type2: "",
+
+        //invisible stuff
+        growthHp: 3,
+        growthEnergy: 3,
+        growthAttack: 2,
+        growthDefense: 2,
+        growthElementalAttack: 5,
+        growthElementalDefense: 2,
+        growthSpeed: 5,
+
+        //move stuff
+        move1: 3,
+        move2: 0,
+        move3: "",
+        move4: ""
+    },
+    {
+        //config stuff
+        name: "Baby Water",
+        image: "water",
+        backImage: "waterBack",
+
+        //visible stuff
+        level: 1,
+        hp: 10,
+        energy: 10,
+        attack: 6,
+        defense: 4,
+        elementalAttack: 8,
+        elementalDefense: 9,
+        speed: 7,
+        type1: "water",
+        type2: "",
+
+        //invisible stuff
+        growthHp: 3,
+        growthEnergy: 3,
+        growthAttack: 2,
+        growthDefense: 2,
+        growthElementalAttack: 4,
+        growthElementalAttack: 5,
+        growthSpeed: 3,
+
+        //move stuff
+        move1: 4,
+        move2: 0,
+        move3: "",
+        move4: ""
+    }
 ]
--- move.js
+++ move.js
@@ -16,4 +16,78 @@ const MOVES = [
         effectChance: 0
     }
     */
+    {
+        //config stuff
+        name: "Slap",
+        icon: "fistIcon",
+        
+        //Visible stuff
+        element: "",
+        damage: 10,
+        description: "slaps the opponent!",
+
+        //hidden stuff
+        effect: "",
+        effectChance: 0
+    },
+    {
+        //config stuff
+        name: "Torch",
+        icon: "fireIcon",
+        
+        //Visible stuff
+        element: "fire",
+        damage: 10,
+        description: "torches the opponent!",
+
+
+        //hidden stuff
+        effect: "",
+        effectChance: 0
+    },
+    {
+        //config stuff
+        name: "Tremor",
+        icon: "earthIcon",
+        
+        //Visible stuff
+        element: "earth",
+        damage: 10,
+        description: "sends shockwaves through the earth!",
+
+
+        //hidden stuff
+        effect: "",
+        effectChance: 0
+    },
+    {
+        //config stuff
+        name: "Gale",
+        icon: "windIcon",
+        
+        //Visible stuff
+        element: "wind",
+        damage: 10,
+        description: "blows a gust at the opponent!",
+
+
+        //hidden stuff
+        effect: "",
+        effectChance: 0
+    },
+    {
+        //config stuff
+        name: "Splash",
+        icon: "waterIcon",
+        
+        //Visible stuff
+        element: "water",
+        damage: 10,
+        description: "splashes the opponent!",
+
+
+        //hidden stuff
+        effect: "",
+        effectChance: 0
+    }
 ]
