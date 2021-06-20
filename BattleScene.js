7f3d51fcb73a9a7b15ad18b39bbb65a59705cbe6
Joshua Beaty <joshua.a.beaty@gmail.com>
2021-06-20 16:26:28 -0500

    Update BattleScene.js added enemy and player object, derived objects from monster and move.js

--- BattleScene.js
+++ BattleScene.js
@@ -1,13 +1,18 @@
 class BattleScene extends Phaser.Scene {
     constructor(){
         super('BattleScene');
-        this.enemyMonster = {
+        this.enemyMonster = null;
+        this.enemyMove1 = null;
+        this.enemyMove2 = null;
+        this.enemyMove3 = null;
+        this.enemyMove4 = null;
 
-        };
+        this.playerMonster = null;
+        this.playerMove1 = null;
+        this.playerMove2 = null;
+        this.playerMove3 = null;
+        this.playerMove4 = null;
 
-        this.playerMonster = {
-
-        };
     }
 
     //shouldn't need this
@@ -22,8 +27,43 @@ class BattleScene extends Phaser.Scene {
         });
         //play audio
         battleBGM.play();
+        //get a random number
+        let monsterIndex = Math.floor(Math.random() * MONSTERS.length);
+
+        //Set Random Opponent Monster
+        this.enemyMonster = MONSTERS[monsterIndex];
+        
+        //set moves from list of moves based on the index provided by the monster.js
+        this.enemyMove1 = MOVES[this.enemyMonster.move1];
+        if(this.enemyMonster.move2 != "")
+            this.enemyMove2 = MOVES[this.enemyMonster.move2];
+        
+        if(this.enemyMonster.move3 != "")
+            this.enemyMove3 = MOVES[this.enemyMonster.move3];
+
+        if(this.enemyMonster.move4 != "")
+            this.enemyMove4 = MOVES[this.enemyMonster.move4];
+
+        //Set Player monster, preferably from signal manager.
+        monsterIndex = Math.floor(Math.random() * MONSTERS.length); //currently a random monster
+        this.playerMonster = MONSTERS[monsterIndex];
+
+        //set moves from list of moves based on the index provided by the monster.js
+        this.playerMove1 = MOVES[this.playerMonster.move1];
+        if(this.playerMonster.move2 != "")
+            this.playerMove2 = MOVES[this.playerMonster.move2];
+        
+        if(this.playerMonster.move3 != "")
+            this.playerMove3 = MOVES[this.playerMonster.move3];
+
+        if(this.playerMonster.move4 != "")
+            this.playerMove4 = MOVES[this.playerMonster.move4];
+        
+        //create display
+
+
         //make a button for title scene
-        let titleBtn = this.add.rectangle(225,400, 200, 100, 0x0000FF);
+        let titleBtn = this.add.rectangle(20,20, 20, 20, 0x0000FF);
         titleBtn.setOrigin(.5);
         titleBtn.setInteractive();
         titleBtn.on('pointerdown', () => {
